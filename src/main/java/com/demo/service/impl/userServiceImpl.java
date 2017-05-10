c3d93b3bd57a6e65fa0fc5edfeed50fa1b134d21
jisixiao <jisixiao23@sina.com>
2017-05-10 21:58:30 +0800

    整理

--- src/main/java/com/demo/service/impl/userServiceImpl.java
+++ src/main/java/com/demo/service/impl/userServiceImpl.java
@@ -1,12 +1,34 @@
 package com.demo.service.impl;
 
+import com.demo.mapper.userMapper;
 import com.demo.po.User;
+import com.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Coding......
  * Created by D.SR on 2017/2/20.
  */
-public interface userServiceImpl {
+@Service
+public class UserServiceImpl implements UserService {
 
-    public User getUser(String userName);
+    @Resource
+    userMapper mapper;
+
+
+    public User getUser(String userName) {
+        return mapper.getUser(userName);
+    }
+
+    public List<User> getAllUser() {
+        return mapper.getAllUser();
+    }
+
+    public List<User> pagedQueryUser(Map<String, Object> map) {
+        return mapper.pagedQueryUser(map);
+    }
 }
