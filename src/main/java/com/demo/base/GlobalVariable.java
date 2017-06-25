37adecf90cdf13d1fa110f42d8aaebac6a8cd807
jisixiao <jisixiao23@sina.com>
2017-06-25 22:20:47 +0800

    全局 static final String的类

---
+++ src/main/java/com/demo/base/GlobalVariable.java
@@ -0,0 +1,15 @@
+package com.demo.base;
+
+/**
+ * 全局static信息
+ *
+ * @author ju_SZ
+ * @create 2017-06-25 19:02
+ **/
+public class GlobalVariable {
+
+
+    public static final String SESSION_USERNAME="userName";
+
+
+}
